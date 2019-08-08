c011b8ecb60b7b5873d2cc80752bf9bc1f2e4db7
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-08 02:29:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,19 +1,9 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-       name: "Lord Montague",
-        age: "53" 
-        },
-      :matriarch => {
-name: "Lady Montague", 
-age: "54"
-        },
-      :hero => {
-name: "Romeo",
-age: "15",
-status: "alive"
-        },
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    }, 
    :capulet => {
