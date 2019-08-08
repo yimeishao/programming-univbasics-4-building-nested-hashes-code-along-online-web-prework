6809d65b3db2cdab2385b0e15057fb64c716acbc
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-08 02:22:40 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,8 +7,10 @@ def second_challenge
 :hero_friends => []
 }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+:patriarch => {}, 
+:matriarch => {},
+:heroine => {},
+:heroine_friends => []
+      }
+  }
 end
